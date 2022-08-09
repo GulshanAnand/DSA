eedc4ec57683d387168dfcef6ae58a2796596460
Gulshan Anand <gulshan2052@gmail.com>
2022-08-09 10:55:30 +0530

    binary exponentiation

---
+++ binary_exponentiation.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+long long binpow(long long a, long long b) {
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+
+long long binpowmod(long long a, long long b, long long m) {
+    a %= m;
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return res;
+}
+
+int main(){
+    return 0;
+}
